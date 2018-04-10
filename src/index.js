4de2cea047cf328ff95054be29dce288d85498b7
Jeff Wang <jeff@Jeffs-MacBook-Pro.local>
2018-04-10 11:32:07 -0400

    junow it is spitting out new divss

--- src/index.js
+++ src/index.js
@@ -5,4 +5,18 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+
+let theArray=[]
+function tick() {
+  theArray.push(Math.floor(Math.random()*50))
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {theArray.map(d=>(<div>the number is {d}</div>))}.</h2>
+    </div>
+  );
+  ReactDOM.render(element, document.getElementById('root'));
+}
+//  new  Constructor is important.  Otherwise, will stop
+setInterval(tick, 1000);
